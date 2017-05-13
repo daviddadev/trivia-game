03f198d3b608f20d1c8b53e3f3de2a836439195f
David Soto <soto.davidj@gmail.com>
2017-05-12 18:56:15 -0700

    my sad homework

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+//test app.js
+// console.log("hello");
+
+// MAKE START BUTTON DISAPPEAR //
+
+var startGame = "start"; 
+
+
+// START TIMER AFTER START GAME CLICK //
+var setTime = 0;
+
+// display after player made choices
+
+var correctGuess = 0;
+
+
+var incorrectGuess = 0;
+
+var tallyScore = 0;
+
+
+//RESET GAME AFTER GAME OVER
+var resetGame = 0;
