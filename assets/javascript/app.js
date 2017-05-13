9987d64d2371cf715956dfcde29afbc62bb6700f
David Soto <soto.davidj@gmail.com>
2017-05-13 12:18:56 -0700

    start button tweaked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,21 +3,32 @@
 
 // MAKE START BUTTON DISAPPEAR //
 
-var startGame = "start"; 
+// var startGame = "start"; 
 
 
-// START TIMER AFTER START GAME CLICK //
-var setTime = 0;
+// // START TIMER AFTER START GAME CLICK //
+// var setTime = 0;
 
-// display after player made choices
+// // display after player made choices
 
-var correctGuess = 0;
-
-
-var incorrectGuess = 0;
-
-var tallyScore = 0;
+// var correctGuess = 0;
 
 
-//RESET GAME AFTER GAME OVER
-var resetGame = 0;
+// var incorrectGuess = 0;
+
+// var tallyScore = 0;
+
+
+// //RESET GAME AFTER GAME OVER
+// var resetGame = 0;
+
+$('#start-btn').click(function(){
+	$('#start-btn').hide();
+	$('#main-content').show();
+});
+
+// function begin {
+// 	$('#start').hide();
+// 	$('#main-content').show();
+
+// }
